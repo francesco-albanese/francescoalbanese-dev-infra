72e40a5a289b1c8b6889d6eb135bb236c110bd95
Francesco Albanese <albanesefrancesco.af@gmail.com>
2026-01-04 23:56:11 +0000

    refactor: remove assume_role from environmental provider
    Co-authored-by: Francesco Albanese <albanesefrancesco.af@gmail.com>

--- terraform/environmental/providers.tf
+++ terraform/environmental/providers.tf
@@ -1,13 +1,6 @@
 provider "aws" {
   region = var.region
 
-  dynamic "assume_role" {
-    for_each = var.account_id != "" ? [1] : []
-    content {
-      role_arn = "arn:aws:iam::${var.account_id}:role/terraform"
-    }
-  }
-
   default_tags {
     tags = {
       "franco:terraform_stack" = "francescoalbanese-dev-infra"
