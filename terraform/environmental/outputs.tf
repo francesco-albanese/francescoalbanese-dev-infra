11ad77863a803bb6e7839a26028d8639ee34cc9e
Francesco Albanese <albanesefrancesco.af@gmail.com>
2026-04-02 00:20:56 +0100

    feat: add ACM cert for CloudFront with DNS validation
    - acm.tf: us-east-1 cert with Route53 DNS validation
    - providers.tf: us-east-1 provider alias
    - outputs.tf: acm_certificate_arn output
    
    Co-authored-by: Francesco Albanese <albanesefrancesco.af@gmail.com>

--- terraform/environmental/outputs.tf
+++ terraform/environmental/outputs.tf
@@ -7,3 +7,8 @@ output "nameservers" {
   description = "Nameservers to configure in Porkbun"
   value       = aws_route53_zone.main.name_servers
 }
+
+output "acm_certificate_arn" {
+  description = "ACM certificate ARN (us-east-1, for CloudFront)"
+  value       = aws_acm_certificate.main.arn
+}
--- terraform/environmental/providers.tf
+++ terraform/environmental/providers.tf
@@ -9,3 +9,17 @@ provider "aws" {
     }
   }
 }
+
+# ACM certificates for CloudFront must be in us-east-1
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      "franco:terraform_stack" = "francescoalbanese-dev-infra"
+      "franco:managed_by"      = "terraform"
+      "franco:environment"     = var.account_name
+    }
+  }
+}
